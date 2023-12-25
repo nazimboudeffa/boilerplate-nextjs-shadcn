22a242a41d24d68724345d39608860978911ae63
Nazim Boudeffa <boudeffa@gmail.com>
2023-12-25 17:58:33 +0100

    Added about

---
+++ src/app/about/page.tsx
@@ -0,0 +1,24 @@
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
+import { fontHeading } from "@/lib/fonts"
+
+export default function AboutPage() {
+    return (
+        <>
+            <Header />
+            <main>
+                <div className="mt-10 flex flex-col items-center gap-10 text-center">
+                    <h1
+                        className={`text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl ${fontHeading.variable}`}
+                    >
+                        About
+                    </h1>
+                    <p className="max-w-[500px] text-lg text-muted-foreground sm:text-xl">
+                        About this project
+                    </p>
+                </div>
+            </main>
+            <Footer />
+        </>
+    )
+}
